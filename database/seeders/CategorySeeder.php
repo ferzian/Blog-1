8375a37540469a7094f624fedf33816cce4dd223
ferzian <m.ferzian09@gmail.com>
2024-06-24 14:44:44 +0700

    ui blog

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Category::factory(3)->create();
+
+
+        Category::create([
+            'name' => 'UI/UX',
+            'slug' => 'ui-ux',
+            'color' => 'red'
+        ]);
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design',
+            'color' => 'green'
+        ]);
+        Category::create([
+            'name' => 'Machine Learning',
+            'slug' => 'machine-learning',
+            'color' => 'blue'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/posts', function () {
+    $posts = Post::with('author', 'category')->latest()->get();
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+});
+
+Route::get('/posts/{post:slug}', function (Post $post) {
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user:username}', function (User $user) {
+    $posts = $user->posts->load('category', 'author');
+
+    return view('posts', ['title' => count($user->posts) .' Articles by: ' . $user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    $posts = $category->posts->load('category', 'author');
+
+    return view('posts', ['title' => count($category->posts) . ' Articles in: ' . $category->name, 'posts' => $posts]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About', 'nama' => 'M. Ferzian Safaridzaldi']);
 });
